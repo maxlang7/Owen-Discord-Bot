70116c58ce341011aa8ba6b964bc6cd56505b9fa
Max Langhorst <maxanna@Maxs-Air.home.langhorst.com>
2020-07-28 18:39:22 -0400

    When a server member logs in, it greets them in a random language.

--- bot.py
+++ bot.py
@@ -1,14 +1,18 @@
 # bot.py
 import os
-
+import random
 import discord
 from dotenv import load_dotenv
-
+from discord import Status
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
+f = open(r'greetings.txt')
+greetings = f.readlines()
+f.close()    
+
 
 @client.event
 async def on_ready():
@@ -19,4 +23,22 @@ async def on_ready():
         )
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
+
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Greetings {member.name}, greatness awaits you in this land of promise.'
+    )
+
+
+@client.event
+async def on_member_update(member_before, member_after):
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    if member_before.status == Status.offline and member_after.status == Status.online:
+        greeting = random.choice(greetings).rstrip()
+        channel = discord.utils.get(client.get_all_channels(), guild__name="maxlang's server", name='general')
+        await channel.send(f'{greeting}, {member_after.name} welcome back to {guild.name}')
+#TO DO wait 30 secs and then delete message
 client.run(TOKEN)
